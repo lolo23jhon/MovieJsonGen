420d67b6a6620dc275d1dea7a32bfb020b764161
lolo23jhon <lolo23jhon@gmail.com>
2020-05-12 09:45:22 -0700

    Made some progress in genereate_file_content().

--- gen.py
+++ gen.py
@@ -3,9 +3,9 @@ import argparse
 from random import choice, randrange
 
 
-class Video:
-
-    VIDEO_NAMES = []
+class VideoGenerator:
+    #####################################################
+    #####################################################
 
     VIDEO_TYPE = {
         "movie": {
@@ -15,9 +15,6 @@ class Video:
             "id_base": "E{episode_num}S{season_num}S{global_entry_num}G{genre}"
         }
     }
-
-    NUM_SEASONS = (1, 12)
-    NUM_RATINGS = (0, 251)
 
     LENGTH = {
         "movie": (5400, 9000),
@@ -45,40 +42,38 @@ class Video:
         "episode_num": 1,
     }
 
-    def generate_file_content(num_movies, num_series):
-        for i in range(num_movies){
-            movie = MOVIE_TEMPLATE.copy()
-            movie[]
+    NUM_SEASONS = (1, 12)
+    NUM_EPISODES_PER_SEASON = (5, 30)
+    NUM_RATINGS = (0, 251)
 
-        }
+    #####################################################
+    #####################################################
 
-    @staticmethod
-    def _generate_id(self, video){
-        vt = video["type"]
-        id_args = {
-            "global_entry_num": self._global_id_num,
-            "genre" = GENRE[video["genre"]]
-        }
-
-        if (vt == "episode"){
-            id_args["episode_num"] = video["episode_num"]
-            id_args["season_num"] = video["season"]
-        }
-
-        id = VIDEO_TYPE[vt]["id_base"].format(**id_args)
-        self._global_id_num += 1
-        return id
-    }
-
+    #####################################################
     def __init__(self, video_names_file):
         videos_json = {}
         with open(video_names_file, "r") as file:
             videos_json = json.load(file)
 
         self._video_names = [name for name in videos_json["names"]]
-        self._last_id = 1
+        self._global_id_num = 1
 
-    def generate_video(video_type, name=choice(VIDEO_NAMES), genre=choice(GENRE), extra_args):
+    # TODO: implement generate_file_content
+    #####################################################
+    def generate_file_content(num_movies, num_series):
+        videos = []
+        for t, num in videos_dict.itesm():
+            if t == "movie"
+            for _ in range(num):
+                videos.append(generate_video(video_type, video_args))
+
+            elif vt == "episode":
+                for i in range(num):
+                    num_ssns = randrange(*NUM_SEAONS)
+                    num_ssns_eps = randrange(*NUM_EPISODES_PER_SEASON)
+
+    #####################################################
+    def generate_video(self, video_type, name=choice(VIDEO_NAMES), genre=choice(GENRE), extra_args):
         if video_type not in VIDEO_TYPE:
             raise ValueError(
                 "Video type \"{}\" does not exist.".format(video_type))
@@ -91,12 +86,30 @@ class Video:
         new_video["duration"] = randrange(*LENGTH[video_type])
         new_video["ratings"] = [rating for rating in randrange(*NUM_RATINGS)]
         new_video["duration"] = randrange(*LENGTH[video_type])
-        new_video["id"] = _generate_id(new_video)
 
-        for key, val in extra_args.val():
+        for key, val in extra_args.items():
             new_video[key] = val
 
+        new_video["id"] = _generate_id(new_video)
+
         return new_video
+
+    #####################################################
+    def _generate_id(self, video)
+    vt = video["type"]
+    id_args = {
+        "global_entry_num": self._global_id_num,
+        "genre" = GENRE[video["genre"]]
+    }
+
+    if (vt == "episode"){
+        id_args["episode_num"] = video["episode_num"]
+        id_args["season_num"] = video["season"]
+    }
+
+    id = VIDEO_TYPE[vt]["id_base"].format(**id_args)
+    self._global_id_num += 1
+    return id
 
 
 if __name__ == "__main__":
